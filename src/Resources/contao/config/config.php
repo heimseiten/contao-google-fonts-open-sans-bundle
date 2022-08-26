b141d8f4f589d709d090c8d35f6fe4c12aa4c398
Niels <info@heimseiten.de>
2022-08-26 10:43:41 +0200

    removed if (TL_MODE == 'FE') {

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,5 +1,3 @@
 <?php
 
-if (TL_MODE == 'FE') {
-    $GLOBALS['TL_HEAD'][] = '<link rel="preload" href="/bundles/heimseitencontaogooglefontsopensans/css/contao-google-fonts-open-sans-bundle.css" as="style" onload="this.onload=null; this.rel=\'stylesheet\';">';
-}
+$GLOBALS['TL_HEAD'][] = '<link rel="preload" href="/bundles/heimseitencontaogooglefontsopensans/css/contao-google-fonts-open-sans-bundle.css" as="style" onload="this.onload=null; this.rel=\'stylesheet\';">';
